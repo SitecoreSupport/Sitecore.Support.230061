f0ca353026304a0ff8a4ba7a235688dc593a2c02
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-07-16 16:23:25 +0300

    deafult styles

---
+++ src/Sitecore.Support.230061/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.230061")]
+[assembly: AssemblyProduct("Sitecore.Support.230061")]
+[assembly: ComVisible(false)]
